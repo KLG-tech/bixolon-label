ace6d566afe289e24c6370045077f2c82f7e6a2b
KLGROUP\181100.YUDISTIRA <yudistira.sujarwo@kawanlamacorp.com>
2024-07-03 23:11:33 +0700

    feature: - move from cache

---
+++ README.md
@@ -0,0 +1,13 @@
+# bixolon_label
+
+Bixolon Printer label using Bixolon Android SDK V2.2.5
+and libcommon V1.2.6
+
+## Limitation
+- Implemented on android only
+- Must connect before print, and disconnect after print
+
+## TODO List
+- Another Functionality from Android SDK
+- IOS SDK
+- Web SDK
---
+++ lib/bixolon_label_method_channel.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+import 'bixolon_label_platform_interface.dart';
+
+/// An implementation of [BixolonLabelPlatform] that uses method channels.
+class MethodChannelBixolonLabel extends BixolonLabelPlatform {
+  /// The method channel used to interact with the native platform.
+  @visibleForTesting
+  final methodChannel = const MethodChannel('bixolon_label');
+
+  @override
+  Future<bool?> connectIp(Map<String, dynamic> param) async {
+    final result = await methodChannel.invokeMethod<bool>('connectIp', param);
+    return result;
+  }
+
+  @override
+  Future<String?> printText(Map<String, dynamic> param) async {
+    final result = await methodChannel.invokeMethod<String>('printText', param);
+    return result;
+  }
+
+  @override
+  Future<String?> printImage(Map<String, dynamic> param) async {
+    final result = await methodChannel.invokeMethod<String>('printImage', param);
+    return result;
+  }
+
+  @override
+  Future<bool?> isConnected() async {
+    final result = await methodChannel.invokeMethod<bool>('isConnected');
+    return result;
+  }
+
+  @override
+  Future<bool?> disconnect() async {
+    final result = await methodChannel.invokeMethod<bool>('disconnect');
+    return result;
+  }
+
+  @override
+  Future<bool?> connectUsb() async {
+    try {
+      final result = await methodChannel.invokeMethod<bool>('connectUsb');
+      return result;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> requestUsbPermissionDialog() async {
+    try {
+      await methodChannel.invokeMethod<void>("requestUsbPermission");
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
